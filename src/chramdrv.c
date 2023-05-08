b3d03ff16fec84dd749060e680f4990bd9af1f6d
tantanGH <tantangh68@gmail.com>
2023-05-08 19:34:55 +0900

    supported RAMDISK_DRIVE env var.

--- src/chramdrv.c
+++ src/chramdrv.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <doslib.h>
 
+#define ENV_VAR "RAMDISK_DRIVE"
+
 int32_t main(int32_t argc, uint8_t* argv[]) {
 
   int32_t rc = 1;
@@ -12,12 +14,21 @@ int32_t main(int32_t argc, uint8_t* argv[]) {
     if (GETDPB(i + 1, &dpb) >= 0) {
       //printf("drive %d ... id = %d\n", i + 1, dpb.id);
       if (dpb.id == 249) {
+        uint8_t drive_letter[] = " :";
+        drive_letter[0] = 'A' + i;
+        printf("Found RAMDISK ... %s\n", drive_letter);
+        if (SETENV(ENV_VAR, NULL, drive_letter) >= 0) {
+          printf("Set environment variable RAMDISK_DRIVE=%s\n", drive_letter);
+        }
         CHGDRV(i);
-        printf("RAMDISK Drive ... %c:\n", 'A' + i);
         rc = 0;
         break;
       }
     }
+  }
+
+  if (rc != 0) {
+    SETENV(ENV_VAR, NULL, NULL);
   }
 
   return rc;
